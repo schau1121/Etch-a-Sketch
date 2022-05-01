aef09468db62e12e0d40b6a60ef563d29b7ca623
schau1121 <simon.chau1121@gmail.com>
2022-04-30 17:03:05 -0700

    Implement logic for changing canvas size

--- etch.js
+++ etch.js
@@ -1,12 +1,36 @@
+let numRows = 16;
+let color = "#000000";
 const canvas = document.querySelector(".canvas");
-console.log(canvas);
+const size_output = document.querySelector("#canvas-size");
+const size_input = document.querySelector(".slider");
 
 function loadCanvas(numRows) {
-    let numSquares = numRows ** 2;
-    for(let i = 0; i < numSquares; i++) {
-        let pixel = document.createElement("div");
-        pixel.classList.add("pixel");
-        pixel.id = i + 1;
-        canvas.appendChild(pixel);
+    canvas.style.setProperty("--grid-rows", numRows);
+    canvas.style.setProperty("--grid-cols", numRows);
+    for(let i = 0; i < (numRows ** 2); i++) {
+        let cell = document.createElement("div");
+        cell.className = "grid-item";
+        cell.id = i + 1;
+        canvas.appendChild(cell);
     }
-}
+};
+
+function clearCanvas() {
+    while(canvas.firstChild) {
+        canvas.removeChild(canvas.firstChild);
+    }
+};
+
+loadCanvas(numRows);
+
+size_input.addEventListener("input", () => {
+    numRows = size_input.valueAsNumber;
+    size_output.textContent = "Size of canvas: " + numRows + "x" + numRows;
+    clearCanvas();
+    loadCanvas(numRows);
+});
+
+const pixels = document.querySelectorAll(".grid-item");
+pixels.forEach(pixel => pixel.addEventListener("hover", (e) => {
+    console.log(e);
+}));
